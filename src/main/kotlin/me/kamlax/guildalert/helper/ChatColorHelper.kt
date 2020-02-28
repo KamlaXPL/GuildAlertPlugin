b775c6dd0ef1970585700d6cb6099eb414828b25
KamlaX <40739398+KamlaXPL@users.noreply.github.com>
2020-02-28 23:12:45 +0100

    Delete ChatColorHelper.kt

--- src/main/kotlin/me/kamlax/guildalert/helper/ChatColorHelper.kt
+++
@@ -1,9 +0,0 @@
-package me.kamlax.guildalert.helper
-
-import org.bukkit.ChatColor
-
-object ChatColorHelper {
-    fun fixColor(message: String) = ChatColor.translateAlternateColorCodes('&', message
-        .replace(">>", "»")
-        .replace("<<", "«"))
-}
